44a03198adc1da5bbf47f85004fb5bb58e333b72
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-11-25 09:16:58 +0100

    // Version updated to v1.5.6

--- blocklink.php
+++ blocklink.php
@@ -36,7 +36,7 @@ class BlockLink extends Module
 	{
 		$this->name = 'blocklink';
 		$this->tab = 'front_office_features';
-		$this->version = '1.5.5';
+		$this->version = '1.5.6';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
