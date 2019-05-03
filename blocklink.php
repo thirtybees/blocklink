4d3c29e7d9a256dcac8530e93ab7ce0099782421
Markus Hitter <mah@jump-ing.de>
2019-05-04 01:30:19 +0200

    Replace _PS_VERSION_ with _TB_VERSION_ to pass validation.

--- blocklink.php
+++ blocklink.php
@@ -23,7 +23,7 @@
  * PrestaShop is an internationally registered trademark of PrestaShop SA.
  */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class BlockLink extends Module
